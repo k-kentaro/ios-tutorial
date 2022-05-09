d427121322bb0d24c7062701d78e828d2f69e8b3
kentaro kimoto <exiler2411k@gmail.com>
2022-05-09 15:18:53 +0900

    feature/tutorial3/STYLEHAUS

--- tutorial/tutorial/Copy/STYLEHAUS/STYLEHAUS.swift
+++ tutorial/tutorial/Copy/STYLEHAUS/STYLEHAUS.swift
@@ -9,13 +9,36 @@ import UIKit
 
 final class STYLEHAUSViewController: UIViewController {
     
+    private let listTableViewCell = "ListTableViewCell"
+    private let listModel = ListModel.createModel()
+    
+    @IBOutlet private weak var listTableView: UITableView! {
+        didSet {
+            listTableView.register(UINib(nibName: listTableViewCell, bundle: nil), forCellReuseIdentifier: listTableViewCell)
+            listTableView.delegate = self
+            listTableView.dataSource = self
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.isHidden = true
-        
     }
     
+}
+
+extension STYLEHAUSViewController: UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        listModel.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = listTableView.dequeueReusableCell(withIdentifier: listTableViewCell, for: indexPath) as? ListTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.configure(model: listModel[indexPath.row])
+        return cell
+    }
     
     
 }
---
+++ tutorial/tutorial/Copy/STYLEHAUS/View/ListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListTableViewCell.swift
+//  tutorial
+//
+//  Created by 木元健太郎 on 2022/05/09.
+//
+
+import UIKit
+
+final class ListTableViewCell: UITableViewCell {
+    
+    @IBOutlet private weak var articleImage: UIImageView!
+    @IBOutlet private weak var articleText: UITextView!
+    @IBOutlet private weak var articleCategory: UILabel!
+    @IBOutlet private weak var writerName: UILabel!
+    
+    
+    func configure(model: ListModel) {
+        articleImage.image = model.articleImage
+        articleText.text = model.articleText
+        articleCategory.text = model.articleCategory
+        writerName.text = model.writerName
+    }
+}
